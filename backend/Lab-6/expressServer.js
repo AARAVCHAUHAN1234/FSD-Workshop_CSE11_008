cd3ea3d2e62d32981f566fc2ad5218fe3f5c3a34
AARAVCHAUHAN1234 <aarav.25b01010006@abes.ac.in>
2026-09-10 15:41:27 +0530

    express

--- backend/Lab-6/expressServer.js
+++ backend/Lab-6/expressServer.js
@@ -1,4 +1,48 @@
 // npm install express dotenv
 import express from "express";
+const userData={
+    id:1,
+    name:"Aarav",
+    age:19
+};
+const port=3000;
+const app=express();    // create app is used to act as a instance for express.
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`);
+})
+//using get
+app.get("/msg",(req,res)=>{
+    res.end("Welcome to the express");
 
-const app=express();
+});
+app.get("/user",(req,res)=>{
+    res.end(JSON.stringify(userData));
+});
+app.get("/user/:id",(req,res)=>{
+    
+    const id=1;
+    const f=userData.find(id);
+    res.end(JSON.stringify(f));
+});
+//using post
+app.post("/create",(req,res)=>{
+    try{
+    const { id,name, age}=req.body;
+    const data={
+        id:id,
+        name:name,
+        age:age
+    };
+    userData.push(data);
+    res.end("Data added");
+}
+catch(err){
+    console.log(err);
+    res.end(err);
+}
+
+});
+
+//using put
+//using delete
+
